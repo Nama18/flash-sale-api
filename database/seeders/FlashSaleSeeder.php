db3fc5745222c64e1ecee3dc2e09d78135512f09
Nama18 <nathanelmario@gmail.com>
2026-06-10 12:41:41 +0700

    feat: add FlashSaleSeeder with sample products

---
+++ database/seeders/FlashSaleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class FlashSaleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Product::create([
+            'name'             => 'Sepatu Nike Flash Sale',
+            'price'            => 1_200_000,
+            'flash_sale_price' => 299_000,
+            'stock'            => 10,
+        ]);
+
+        Product::create([
+            'name'             => 'Kaos Polos Premium',
+            'price'            => 150_000,
+            'flash_sale_price' => null,
+            'stock'            => 100,
+        ]);
+    }
+}
